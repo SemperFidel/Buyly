586d6dbb0908855314b924e76fb4e69c61e88f6d
SemperFidel <zandrekh.40000@gmail.com>
2025-11-22 20:28:27 +0300

    Фикс пустого конфига

--- server/src/main/kotlin/ru/test/storeapp/Application.kt
+++ server/src/main/kotlin/ru/test/storeapp/Application.kt
@@ -2,14 +2,21 @@ package ru.test.storeapp
 
 import io.ktor.server.application.*
 import io.ktor.server.cio.CIO
+import io.ktor.server.config.yaml.YamlConfigLoader
 import io.ktor.server.engine.*
 import ru.test.storeapp.config.configureAPI
 import ru.test.storeapp.config.configureDI
 import ru.test.storeapp.config.configureSerialization
 
 fun main() {
-    embeddedServer(CIO, port = 8080, host = "127.0.0.1", module = Application::module)
-        .start(wait = true)
+    val yamlConfig = YamlConfigLoader().load("application.yaml")
+        ?: throw IllegalStateException("Cannot load YAML config")
+
+    embeddedServer(
+        CIO,
+        host = yamlConfig.property("ktor.deployment.host").getString(),
+        port = yamlConfig.property("ktor.deployment.port").getString().toInt()
+    ) { module() }.start(wait = true)
 }
 
 fun Application.module() {
